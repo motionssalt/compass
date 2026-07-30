a299bcc6c229bcb6b59d7a487810d1966c2d0aa7
Jumbo Fubara <motionssalt@gmail.com>
2026-07-30 16:51:42 +0100

    bot: push zip via secrets pusher

--- src/handlers/cron.ts
+++ src/handlers/cron.ts
@@ -4,6 +4,7 @@
 import type { Env } from '../types/env';
 import { resetDailyQuotas } from '../db/apiKeys';
 import { resetRecurringForDay } from '../db/tasks';
+import { purgeExpiredConfirmations } from '../db/confirmations';
 import { localDateString } from '../utils/time';
 import { log } from '../utils/logger';
 
@@ -13,10 +14,12 @@ export async function handleCron(env: Env): Promise<void> {
 
   const keysReset = await resetDailyQuotas(env.DB, today);
   const tasksReset = await resetRecurringForDay(env.DB, tz);
+  const confirmationsPurged = await purgeExpiredConfirmations(env.DB);
 
   log.info('cron_daily_reset', {
     date: today,
     api_keys_reset: keysReset,
     recurring_tasks_reset: tasksReset,
+    confirmations_purged: confirmationsPurged,
   });
 }
